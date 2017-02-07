ee1996ed533a56a8f1bacfcf064a93e1d13b7572
Mark Williams <mwilliams@shoprunner.com>
2017-02-07 01:37:49 +0000

    License and classifiers

--- setup.py
+++ setup.py
@@ -13,4 +13,8 @@ setup(
     setup_requires=["cffi>=1.0.0"],
     cffi_modules=["src/_build.py:ffibuilder"],
     install_requires=["cffi>=1.0.0"],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+    ],
+    license="MIT",
 )
