8087964c2f619a15b63b3b18836e1a2a384e1f18
Mark Williams <mwilliams@shoprunner.com>
2017-02-07 01:41:38 +0000

    add dev extra

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setup(
     setup_requires=["cffi>=1.0.0"],
     cffi_modules=["src/_build.py:ffibuilder"],
     install_requires=["cffi>=1.0.0"],
+    extras_require={
+        'dev': ["coverage", "hypothesis>=3.6.1", "pytest"],
+    },
     classifiers=[
         "Programming Language :: Python :: 2.7",
     ],
