a080850158814549d13cba2f2062d345b40e3caa
Mark Williams <mwilliams@shoprunner.com>
2017-03-10 12:38:15 -0800

    implicit relative imports!

--- src/monotone/__init__.py
+++ src/monotone/__init__.py
@@ -1,4 +1,4 @@
 from monotone._api import get_clock_info, monotonic
-from _version import __version__
+from ._version import __version__
 
 __all__ = ["get_clock_info", "monotonic", "__version__"]
--- src/monotone/_version.py
+++ src/monotone/_version.py
@@ -7,5 +7,5 @@ Provides monotone version information.
 
 from incremental import Version
 
-__version__ = Version('monotone', 17, 3, 1)
+__version__ = Version('monotone', 17, 3, 2)
 __all__ = ["__version__"]
